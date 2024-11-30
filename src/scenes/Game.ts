0e7c75a60f6c18f4eaee524b8c9a69ef699540bc
Oleksandr <alexander.hanaychuk@gmail.com>
2024-11-30 15:38:08 +0200

    add input and player

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,35 +1,49 @@
-import { Scene } from 'phaser';
+import { Scene } from "phaser";
+import { Player } from "../Objects/Player";
+import { WASDInput } from "../types/input";
 
-export class Game extends Scene
-{
-    camera: Phaser.Cameras.Scene2D.Camera;
-    background: Phaser.GameObjects.Image;
-    msg_text : Phaser.GameObjects.Text;
+export class Game extends Scene {
+  camera: Phaser.Cameras.Scene2D.Camera;
+  background: Phaser.GameObjects.Image;
+  msg_text: Phaser.GameObjects.Text;
+  player: Player;
+  keys: WASDInput;
 
-    constructor ()
-    {
-        super('Game');
+  constructor() {
+    super("Game");
+  }
+
+  prealod() {}
+
+  create() {
+    this.camera = this.cameras.main;
+    this.camera.setBackgroundColor(0x00ff00);
+
+    this.background = this.add.image(512, 384, "background");
+    this.background.setAlpha(0.5);
+
+    this.player = new Player(
+      this,
+      { x: 512, y: 400 },
+      { width: 60, height: 60 },
+    );
+
+    this.keys = this.input.keyboard?.addKeys("W,S,A,D") as WASDInput;
+
+    // this.input.once("pointerdown", () => {
+    //   this.scene.start("GameOver");
+    // });
+  }
+
+  update(): void {
+    if (this.keys.A.isDown) {
+      console.log("LEFT");
+    } else if (this.keys.D.isDown) {
+      console.log("RIGHT");
+    } else if (this.keys.S.isDown) {
+      console.log("DOWN");
+    } else if (this.keys.W.isDown) {
+      console.log("UP");
     }
-
-    create ()
-    {
-        this.camera = this.cameras.main;
-        this.camera.setBackgroundColor(0x00ff00);
-
-        this.background = this.add.image(512, 384, 'background');
-        this.background.setAlpha(0.5);
-
-        this.msg_text = this.add.text(512, 384, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        });
-        this.msg_text.setOrigin(0.5);
-
-        this.input.once('pointerdown', () => {
-
-            this.scene.start('GameOver');
-
-        });
-    }
+  }
 }
